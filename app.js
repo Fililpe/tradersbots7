8b94a6d71207caeba38051d95dee486358590b3d
Filipe Sousa <90399125+Fililpe@users.noreply.github.com>
2023-12-05 21:11:25 -0300

    .

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const hamburger = document.querySelector(".hamburger")
 const nav = document.querySelector(".nav")
 
+
 hamburger.addEventListener("click", () => nav.classList.toggle("active"))
+
+
+window.addEventListener('scroll', () =>{
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    
+    if (scrollTop > 50) {
+        nav.classList.add('nav-fixed')
+    } else {nav.classList.remove('nav-fixed')}
+})
