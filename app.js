53624f80fd1b957fa1e803e4fc3e93101dd4e751
Filipe Sousa <90399125+Fililpe@users.noreply.github.com>
2023-12-06 21:10:17 -0300

    fix bug, add elements

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const hamburger = document.querySelector(".hamburger")
 const nav = document.querySelector(".nav")
+// let lastScrollTop = 0;
 
 
 hamburger.addEventListener("click", () => nav.classList.toggle("active"))
 
 
-window.addEventListener('scroll', () =>{
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    
-    if (scrollTop > 50) {
-        nav.classList.add('nav-fixed')
-    } else {nav.classList.remove('nav-fixed')}
-})
+window.addEventListener("scroll", () => {
+    const header = document.querySelector(".header");
+  
+    if (window.scrollY > 100) {
+      header.classList.add("nav-fixed");
+    } else {
+      header.classList.remove("nav-fixed");
+    }
+  });
+
+//   window.addEventListener("scroll", () => {
+//     const header = document.querySelector(".header");
+//     const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+  
+//     if (currentScroll > lastScrollTop) {
+//       header.classList.add("nav-fixed");
+//     } else {
+//       header.classList.remove("nav-fixed");
+//     }
+  
+//     lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+//   });
